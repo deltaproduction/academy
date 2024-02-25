e35dcf93edda22731dce2489cea065a151a5e01f
Dzhamaludin Osmanov <osmanov230497@gmail.com>
2024-02-25 20:08:21 +0300

    Group auth views

--- frontend/app/(auth)/register/page.jsx
+++ frontend/app/(auth)/register/page.jsx
@@ -2,22 +2,29 @@
 
 import { useState } from "react";
 
-import styles from "./page.module.scss";
 
 export default function Register() {
   const [name, setName] = useState("")
   const [surname, setSurname] = useState("")
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
 
   const onChangeValue = setStateFunction => e => {
     setStateFunction(e.target.value)
   }
 
+  const onFormSubmit = formData => {
+
+  }
+
   return <div>
-    <form>
+    <form action={onFormSubmit}>
       <div>Создание аккаунта</div>
       <input type="text" placeholder="Имя" value={name} onChange={onChangeValue(setName)}/>
       <input type="text" placeholder="Фамилия" value={surname} onChange={onChangeValue(setSurname)}/>
-      <input type="text" placeholder="Фамилия" value={surname} onChange={onChangeValue(setSurname)}/>
+      <input type="email" placeholder="Email" value={email} onChange={onChangeValue(setEmail)}/>
+      <input type="password" placeholder="Пароль" value={password} onChange={onChangeValue(setPassword)}/>
+      <input type="submit" value="Войти"/>
     </form>
   </div>
 }
