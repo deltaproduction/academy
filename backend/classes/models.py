106a9100873a0332bab80a182ffba41c881e1b8d
Dzhamaludin Osmanov <osmanov230497@gmail.com>
2024-03-03 18:29:32 +0300

    Исправлен nginx

--- backend/classes/models.py
+++ backend/classes/models.py
@@ -7,11 +7,10 @@ User = get_user_model()
 
 
 class Class(models.Model):
-    tutor = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
-    course = models.ForeignKey(Course, null=True, on_delete=models.SET_NULL)
     title = models.CharField("Заголовок", max_length=150, blank=True)
-    teacher_name = models.CharField("ФИО Учителя", max_length=150, blank=True)
-    code = models.CharField("Код", max_length=7, blank=True)
+    tutor = models.ForeignKey(User, null=True, on_delete=models.PROTECT)
+    course = models.ForeignKey(Course, null=True, blank=True, on_delete=models.PROTECT)
+    code = models.IntegerField("Код", blank=True)
 
     class Meta:
         verbose_name = "Класс"
--- backend/courses/models.py
+++ backend/courses/models.py
@@ -5,47 +5,69 @@ User = get_user_model()
 
 
 class Course(models.Model):
-    tutor = models.ForeignKey(User, on_delete=models.CASCADE)
-    title = models.CharField("Заголовок", max_length=150, blank=True)
-    public = models.BooleanField("Опубликован", default=False)
+    DRAFT = 0
+    PUBLISHED = 1
+    STATE_CHOICES = (
+        (DRAFT, 'Черновик'),
+        (PUBLISHED, 'Опубликован')
+    )
+
+    author = models.ForeignKey(User, on_delete=models.PROTECT)
+
+    title = models.CharField('Заголовок', max_length=150)
+    state = models.PositiveSmallIntegerField('Опубликован', choices=STATE_CHOICES, default=DRAFT)
 
     class Meta:
-        verbose_name = "Курс"
-        verbose_name_plural = "Курсы"
+        verbose_name = 'Курс'
+        verbose_name_plural = 'Курсы'
 
 
 class Topic(models.Model):
+    EDUCATIONAL = 0
+    CLASS_WORK = 1
+    INDEPENDENT_WORK = 2
+    TYPE_CHOICES = (
+        (EDUCATIONAL, 'Учебная тема'),
+        (CLASS_WORK, 'Классная работа'),
+        (INDEPENDENT_WORK, 'Самостоятельная работа'),
+    )
+
     course = models.ForeignKey(Course, on_delete=models.CASCADE)
-    title = models.CharField("Заголовок", max_length=150, blank=True)
-    deadline = models.DateTimeField()
-    type = models.PositiveSmallIntegerField("Тип")
+
+    title = models.CharField('Заголовок', max_length=150, blank=True)
+    type = models.PositiveSmallIntegerField('Тип', choices=TYPE_CHOICES, default=EDUCATIONAL)
+
+    start = models.DateTimeField('Дата окончания', null=True, blank=True)
+    end = models.DateTimeField('Дата окончания', null=True, blank=True)
 
     class Meta:
-        verbose_name = "Тема"
-        verbose_name_plural = "Темы"
+        verbose_name = 'Тема'
+        verbose_name_plural = 'Темы'
 
 
 class Task(models.Model):
+
     topic = models.ForeignKey(Topic, on_delete=models.CASCADE)
-    title = models.CharField("Заголовок", max_length=150, blank=True)
-    text = models.CharField("Текст", max_length=150, blank=True)
-    autocheck = models.BooleanField("autocheck", default=False)
-    stdin = models.TextField("stdin")
-    stdout = models.TextField("stdout")
-    samples = models.TextField("samples")
-    tests = models.TextField("tests")
+
+    title = models.CharField('Заголовок', max_length=150, blank=True)
+    text = models.TextField('Текст', blank=True)
+    # autocheck = models.BooleanField('Авто-проверка', default=False)
+    # stdin = models.TextField('stdin')
+    # stdout = models.TextField('stdout')
+    # samples = models.TextField('samples')
+    # tests = models.TextField('tests')
 
     class Meta:
-        verbose_name = "Задание"
-        verbose_name_plural = "Задания"
+        verbose_name = 'Задание'
+        verbose_name_plural = 'Задания'
 
 
 class Solution(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     task = models.ForeignKey(Task, on_delete=models.CASCADE)
-    code = models.TextField("code")
-    status = models.PositiveSmallIntegerField("status")
+    code = models.TextField('code')
+    status = models.PositiveSmallIntegerField('status')
 
     class Meta:
-        verbose_name = "Решение"
-        verbose_name_plural = "Решения"
+        verbose_name = 'Решение'
+        verbose_name_plural = 'Решения'
