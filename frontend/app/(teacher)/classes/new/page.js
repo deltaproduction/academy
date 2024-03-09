32e2874979de8732de5bdd9c8507c4b50fbc88d6
Dzhamaludin Osmanov <osmanov230497@gmail.com>
2024-03-09 18:51:05 +0300

    Revert "page structure changes"
    This reverts commit 0ea3ed2428ed65d39577e7b8994c964935b53746.

--- frontend/app/(teacher)/classes/new/page.js
+++ frontend/app/(teacher)/classes/new/page.js
@@ -1,16 +1,5 @@
-<<<<<<< Updated upstream
 import Class from "@/app/(teacher)/classes/components/Class";
 
 export default async function Page() {
   return <Class/>;
-=======
-import PageWithSidebar from "@/app/components/PageWithSidebar";
-
-export default function NewClass() {
-    return (
-        <PageWithSidebar title="Класс">
-            новый класс
-        </PageWithSidebar>
-    );
->>>>>>> Stashed changes
 }
--- frontend/app/(teacher)/courses/new/page.js
+++
@@ -1,9 +0,0 @@
-import PageWithSidebar from "@/app/components/PageWithSidebar";
-
-export default function NewCourse() {
-    return (
-        <PageWithSidebar title="Курсы">
-            новый курс
-        </PageWithSidebar>
-    );
-}
