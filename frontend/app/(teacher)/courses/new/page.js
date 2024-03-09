0ea3ed2428ed65d39577e7b8994c964935b53746
osmanof <oc-inbox@inbox.ru>
2024-03-09 14:28:50 +0300

    page structure changes

---
+++ frontend/app/(teacher)/courses/new/page.js
@@ -0,0 +1,9 @@
+import PageWithSidebar from "@/app/components/PageWithSidebar";
+
+export default function NewCourse() {
+    return (
+        <PageWithSidebar title="Курсы">
+            новый курс
+        </PageWithSidebar>
+    );
+}
