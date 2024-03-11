5d81c55d0849950a17e76aad4993bf55d6ae594a
Dzhamaludin Osmanov <osmanov230497@gmail.com>
2024-03-11 13:06:12 +0300

    Some updates

--- frontend/pages/classes/index.js
+++ frontend/pages/classes/index.js
@@ -1,12 +1,12 @@
-import { ClassesApi } from "@/lib/api";
+import { ClassesApi }     from "@/lib/api";
 
-export async function getServerSideProps({query: {next = '/'}, req, res}) {
+export async function getServerSideProps({req, res}) {
   const response = await ClassesApi.list({req, res})
   if (response.ok) {
     const classes = await response.json()
     return {redirect: {destination: classes.length ? `/classes/${classes[0].id}/` : '/classes/new/', permanent: false}}
   }
-  return {props: {next}}
+  return {redirect: {destination: `/login/?next=${req.url}`, permanent: false}}
 }
 
 
--- frontend/pages/courses/index.js
+++ frontend/pages/courses/index.js
@@ -1,12 +1,12 @@
 import { CoursesApi } from "@/lib/api";
 
-export async function getServerSideProps({query: {next = '/'}, req, res}) {
+export async function getServerSideProps({req, res}) {
   const response = await CoursesApi.list({req, res})
   if (response.ok) {
     const courses = await response.json()
     return {redirect: {destination: courses.length ? `/courses/${courses[0].id}/` : '/courses/new/', permanent: false}}
   }
-  return {props: {next}}
+  return {redirect: {destination: `/login/?next=${req.url}`, permanent: false}}
 }
 
 
