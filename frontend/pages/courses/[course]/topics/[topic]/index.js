f89fb5721ef33d80d6f4ba724be80f80479d81fd
Dzhamaludin Osmanov <osmanov230497@gmail.com>
2024-03-13 22:15:01 +0300

    Some fixes

--- frontend/pages/courses/[course]/topics/[topic]/index.js
+++ frontend/pages/courses/[course]/topics/[topic]/index.js
@@ -1,17 +1,26 @@
 import { TopicsApi }                 from "@/lib/api";
 import { getProfileServerSideProps } from "@/lib/utils";
 
-import { isPlainObject } from "next/dist/shared/lib/is-plain-object";
-import AppLayout         from "@/layouts/AppLayout";
+import { isPlainObject }        from "next/dist/shared/lib/is-plain-object";
+import AppLayout                from "@/layouts/AppLayout";
+import styles                   from "@/pages/courses/[course]/index.module.scss";
+import { Sidebar, SidebarItem } from "@/components/Sidebar";
 
-export async function getServerSideProps({query: {topic}, req, res}) {
+export async function getServerSideProps({query: {topic, course}, req, res}) {
   try {
     const {props} = await getProfileServerSideProps({req, res})
 
-    const topicsRes = await TopicsApi.retrieve(topic, {req, res})
-    if (topicsRes.ok) {
-      props.topic = await topicsRes.json()
+    let response = await TopicsApi.retrieve(topic, {req, res})
+    if (response.ok) {
+      props.topic = await response.json()
+    } else {
+
     }
+
+    const topicsRes = await TopicsApi.retrieve(topic, {req, res})
+
+
+    props.courseId = course
 
     return {props}
   } catch (e) {
@@ -20,10 +29,25 @@ export async function getServerSideProps({query: {topic}, req, res}) {
   }
 }
 
-const Topic = ({topic, profile}) => {
+const Layout = ({topics, profile, children, courseId}) => {
   return <AppLayout profile={profile}>
-    {topic.title}
+    <div className={styles.container}>
+      <Sidebar title="Темы" newItemHref={`/courses/${courseId}/topics/new/`}>
+        {topics.map(({id, title}) => (
+          <SidebarItem key={id} href={`/courses/${courseId}/topics/${id}/`}>{title}</SidebarItem>)
+        )}
+      </Sidebar>
+      <div className={styles.content}>
+        {children}
+      </div>
+    </div>
   </AppLayout>
+}
+
+const Topic = ({topic, profile, children, courseId}) => {
+  return <Layout profile={profile}>
+    {topic.title}
+  </Layout>
 }
 
 export default Topic
--- frontend/pages/courses/[course]/topics/index.js
+++ frontend/pages/courses/[course]/topics/index.js
@@ -1,29 +1,20 @@
-import { TopicsApi }                 from "@/lib/api";
-import { getProfileServerSideProps } from "@/lib/utils";
+import { TopicsApi } from "@/lib/api";
 
-import { isPlainObject } from "next/dist/shared/lib/is-plain-object";
-import AppLayout         from "@/layouts/AppLayout";
-
-export async function getServerSideProps({query: {topic}, req, res}) {
-  try {
-    const {props} = await getProfileServerSideProps({req, res})
-
-    const topicsRes = await TopicsApi.retrieve(topic, {req, res})
-    if (topicsRes.ok) {
-      props.topic = await topicsRes.json()
+export async function getServerSideProps({query: {course}, req, res}) {
+  const response = await TopicsApi.list({req, res})
+  if (response.ok) {
+    const topics = await response.json()
+    return {
+      redirect: {
+        destination: topics.length ? `/courses/${course}/topics/${topics[0].id}/` : `/courses/${course}/topics/new/`,
+        permanent: false
+      }
     }
-
-    return {props}
-  } catch (e) {
-    if (isPlainObject(e)) return e
-    throw e
   }
+  return {redirect: {destination: `/login/?next=${req.url}`, permanent: false}}
 }
 
-const Topic = ({topic, profile}) => {
-  return <AppLayout profile={profile}>
-    {topic.title}
-  </AppLayout>
-}
 
-export default Topic
+export default function Page() {
+  return <div></div>
+}
